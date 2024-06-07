725cd7798626417d309a58c97dff043cdcc0788b
Matteo Delfino <matteo.delfino@isi.it>
2024-06-07 17:09:14 +0200

    first contacts survey stub

--- src/editor-example-generators/surveys.ts
+++ src/editor-example-generators/surveys.ts
@@ -1,6 +1,7 @@
 import getBelIntake from './belgium/inf-intake';
 import getBelWeekly from './belgium/inf-weekly';
 import getBelVaccination from './belgium/inf-vaccination';
+import {ContactsDef} from "./external/contacts-survey/src/influenzanet-verdi-contact-extension/surveys/Contacts"
 
 const surveys = [
     {
@@ -9,6 +10,7 @@ const surveys = [
             { name: "intake", survey: getBelIntake() },
             { name: "weekly", survey: getBelWeekly() },
             { name: "vaccination", survey: getBelVaccination() },
+            { name: "contacts", survey: new ContactsDef().getSurvey() },
         ],
         languageCodes: [
             'nl-be',
