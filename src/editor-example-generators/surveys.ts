96ce0579eceb7430d81b3f6c141946ea96d3fc1d
Matteo Delfino <matteo.delfino@isi.it>
2024-09-17 15:43:07 +0200

    add be languages

--- src/editor-example-generators/surveys.ts
+++ src/editor-example-generators/surveys.ts
@@ -1,24 +1,27 @@
-import getBelIntake from './belgium/inf-intake';
-import getBelWeekly from './belgium/inf-weekly';
-import getBelVaccination from './belgium/inf-vaccination';
-import {ContactsDef} from "./external/contacts-survey/src/influenzanet-verdi-contact-extension/surveys/Contacts"
+import getBelIntake from "./belgium/inf-intake";
+import getBelWeekly from "./belgium/inf-weekly";
+import getBelVaccination from "./belgium/inf-vaccination";
+import { ContactsDef } from "./external/contacts-survey/src/influenzanet-verdi-contact-extension/surveys/Contacts";
+
+import { en_BE } from "./external/contacts-survey/src/influenzanet-verdi-contact-extension/languages/en-be";
+import { nl_BE } from "./external/contacts-survey/src/influenzanet-verdi-contact-extension/languages/nl-be";
+import { de_BE } from "./external/contacts-survey/src/influenzanet-verdi-contact-extension/languages/de-be";
+import { fr_BE } from "./external/contacts-survey/src/influenzanet-verdi-contact-extension/languages/fr-be";
 
 const surveys = [
     {
-        instance: 'belgium',
+        instance: "belgium",
         surveys: [
             { name: "intake", survey: getBelIntake() },
             { name: "weekly", survey: getBelWeekly() },
             { name: "vaccination", survey: getBelVaccination() },
-            { name: "contacts", survey: new ContactsDef().getSurvey() },
+            {
+                name: "contacts",
+                survey: new ContactsDef([en_BE, nl_BE, de_BE, fr_BE]).getSurvey(),
+            },
         ],
-        languageCodes: [
-            'nl-be',
-            'fr-be',
-            'de-be',
-            'en',
-        ]
-    }
+        languageCodes: ["nl-be", "fr-be", "de-be", "en"],
+    },
 ];
 
 export default surveys;
